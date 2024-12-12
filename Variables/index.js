a5820d3ec273dfccc05316d3f8d1f38edc364e84
Ankit Kumar <ankitmallik33@gmail.com>
2024-12-12 10:49:03 +0530

    learned variables

---
+++ Variables/index.js
@@ -0,0 +1,31 @@
+// variable 
+// question 1
+
+let a = "ankit"
+let b=6
+console.log(b+a);
+
+// question 2
+
+let a = "mallik"
+console.log(typeof (a));
+
+// question 3
+
+const a1 =
+{
+    name: "ankit",
+    class: 7,
+    teachers: false
+}
+a1['friend']='mallik'
+console.log(a1);
+
+// question 4
+
+const a = {
+    good: "bahut accha",
+    nice: "aur accha"  
+}
+console.log(a['nice']);
+
